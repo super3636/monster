242341bde13837df853588f191641e715f2ddc58
super3636 <ioeioe649764321@gmail.com>
2020-08-11 11:52:12 +0700

    Updates

---
+++ precache-manifest.ee5e2b9b9578a2b85dde3864fc77fd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c3dfe0c2ca46be707fc44a4e66d4a4",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "eb880b39dd486deb8f7e",
+    "url": "/monster/static/css/main.a723c63a.chunk.css"
+  },
+  {
+    "revision": "c29c2a1ae98f9096bf16",
+    "url": "/monster/static/js/2.229663af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.229663af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb880b39dd486deb8f7e",
+    "url": "/monster/static/js/main.462fef96.chunk.js"
+  },
+  {
+    "revision": "5446ca17d6312361b3b2",
+    "url": "/monster/static/js/runtime-main.b7e78b55.js"
+  }
+]);
